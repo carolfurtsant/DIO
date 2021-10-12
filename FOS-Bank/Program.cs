c21c6f3d068036c8a2785062b9b9f3b325488a0d
Ana Caroline <carolfurtadosantos@outlook.com>
2021-10-11 23:07:56 -0300

    Adicionando Projeto

---
+++ FOS-Bank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FOS_Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
