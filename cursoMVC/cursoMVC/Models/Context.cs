cfb7dd3da338dc8c9fee4d59ebf6681626ee7972
Ana Caroline <84164877+carolfurtsant@users.noreply.github.com>
2021-10-22 19:17:14 -0300

    MVC

---
+++ cursoMVC/cursoMVC/Models/Context.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString : @"Server=localhost\SQLEXPRESS;Database=cursoMVC;Trusted_Connection=True");
+        }
+
+        public void SetModified(Categoria categoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetModified(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
